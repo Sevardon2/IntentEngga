3697253378afb47fce31cecb42a90802a1cc799b
Engga Ananda Pratama <pratamaengga83@gmail.com>
2021-11-04 17:43:11 +0700

    Intent Engga
    Latihan Intent

---
+++ app/src/main/java/com/engga/intentengga/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.engga.intentengga
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnMoveActivity: Button
+    private lateinit var btnMoveWithData: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        btnMoveWithData = findViewById(R.id.btn_move_with_data)
+        btnMoveWithData.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v:View?) {
+        if (v != null) {
+            when(v.id) {
+                R.id.btn_move_activity -> run {
+                    val intent = Intent(this, MoveActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn_move_with_data -> run {
+                    val intent = Intent(this, MoveWithDataActivity::class.java)
+                    val bundle = Bundle()
+                    bundle.putString("Nama", "Engga")
+                    bundle.putString("Alamat", "Pakis")
+                    intent.putExtras(bundle)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
